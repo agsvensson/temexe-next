842684a5d0214a8749a4e544d8ee658abcf5a5d5
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2021-02-26 14:47:46 -0300

    Aula 03

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/global.css'
 
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+import { useState } from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <ChallengesProvider>
+    {/* objetos dentro do Provider tem acesso aos dados dentro daquele contexto */}
+    <Component {...pageProps} />
+    </ChallengesProvider>  
+  ) 
 }
 
 export default MyApp
